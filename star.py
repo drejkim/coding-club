4b398699a1449ee489fc51fcd4588255bee310d8
Esther Jun Kim <hello@esthermakes.tech>
2020-07-05 09:46:45 -0700

    Add star.py

---
+++ star.py
@@ -0,0 +1,16 @@
+import turtle
+
+t = turtle.Turtle()
+
+t.forward(100)
+t.right(144)
+t.forward(100)
+t.right(144)
+t.forward(100)
+t.right(144)
+t.forward(100)
+t.right(144)
+t.forward(100)
+t.right(144)
+
+turtle.done()
