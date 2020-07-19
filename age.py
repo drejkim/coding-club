503538b81726798933fa8cdff07d7a9287ae0dfc
Esther Jun Kim <hello@esthermakes.tech>
2020-07-19 07:54:36 -0700

    Add age.py

---
+++ age.py
@@ -0,0 +1,29 @@
+age = 13
+print(age)
+if age == 12:
+    print("I'm a pre-teen!")
+else:
+    print("I'm a teenager!")
+
+age = 8
+print(age)
+if age == 12:
+    print("I'm a pre-teen!")
+elif age == 13:
+    print("I'm a teenager!")
+else:
+    print("Huh?")
+
+age = 13
+print(age)
+if age == 12 or age == 13:
+    print("Are you in middle school?")
+else:
+    print("Huh?")
+
+age = 15
+print(age)
+if age >= 13 and age <= 18:
+    print("You are a teenager, for sure!")
+else:
+    print("Huh?")
